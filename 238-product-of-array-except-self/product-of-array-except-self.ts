d2faff29dd7f3c61f8e1f08711524c3a64a752d3
Arindam Bhattacharyya <134784182+ArindamBhatta@users.noreply.github.com>
2025-12-23 11:52:29 +0530

    Time: 4 ms (87.23%) | Memory: 73 MB (20.33%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -0,0 +1,17 @@
+function productExceptSelf(nums: number[]): number[] {
+  const answer = new Array(nums.length).fill(1);
+    //answer[i] = (product of left side) × (product of right side)
+  let left = 1;
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = left;
+    left *= nums[i];
+  }
+
+  let right = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answer[i] *= right;
+    right *= nums[i];
+  }
+
+  return answer;
+}
